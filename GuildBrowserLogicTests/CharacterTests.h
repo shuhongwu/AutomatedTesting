0db536ebdc375ce615a31c67914f2fd196d51cf3
AlexWu <ws169144@163.com>
2012-11-01 22:09:20 +0800

    添加字符型测试

---
+++ GuildBrowserLogicTests/CharacterTests.h
@@ -0,0 +1,13 @@
+//
+//  CharacterTests.h
+//  GuildBrowser
+//
+//  Created by Alex on 11/1/12.
+//  Copyright (c) 2012 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CharacterTests : SenTestCase
+
+@end
