f33e7a9586cef7c4d2c89d475489ce4fe0986a42
Alex <wushuhong@datang.com>
2012-11-01 17:41:15 +0800

    添加测试代码

---
+++ GuildBrowserLogicTests/WowUtilsTests.h
@@ -0,0 +1,13 @@
+//
+//  WowUtilsTests.h
+//  GuildBrowser
+//
+//  Created by Alex Wu on 11/1/12.
+//  Copyright (c) 2012 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WowUtilsTests : SenTestCase
+
+@end
